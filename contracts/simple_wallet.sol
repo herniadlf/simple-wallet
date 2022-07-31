c8107a44fd47609d995d0a955b2a674d38502299
Hernán de la Fuente <hernan.de@mercadolibre.com>
2022-07-31 17:45:25 -0300

    add allowance account

--- contracts/simple_wallet.sol
+++ contracts/simple_wallet.sol
@@ -18,5 +18,52 @@ import "@openzeppelin/contracts/access/Ownable.sol";
  */
 contract SimpleWallet is Ownable {
 
+    /* Events */
+
+    /**
+     * @dev Emitted when this contract receives funds.
+     * @param _sender The address that sends funds to the contract.
+     * @param _amount The amount that _sender sends to the contract.
+     */
+    event FundsReceived(address indexed _sender, uint _amount);
+
+    /**
+     * @dev Emitted when a new address is declared for withdrawing funds from the contract.
+     * @param _allowedAccount The new address that is allowed to withdraw funds.
+     */
+    event NewAccountAllowed(address indexed _allowedAccount);
+
+    /* Structs */
+
+
+    /* Storage */
+
+    mapping(address => bool) private allowedAccounts;
+    
+    /* External and public */
+
+    /**
+     * @dev Fallback function that allow this contract to receive ether.
+     */
+    receive() external payable {
+        emit FundsReceived(msg.sender, msg.value);
+    }
+
+    /**
+     * @dev Add a new account the allowance to withdraw funds.
+     * @param _allowedAccount The new address that is allowed to withdraw funds.
+     */
+    function addAllowedAccount(address _allowedAccount) onlyOwner public {
+        allowedAccounts[_allowedAccount] = true;
+        emit NewAccountAllowed(_allowedAccount);
+    }
+
+    /**
+     * @dev Check if an accounts is allowed to withdraw funds from the contract.
+     * @param _address The address to check if its allowed to withdraw funds from the contract.
+     */
+    function isAllowed(address _address) public view returns (bool) {
+        return allowedAccounts[_address];
+    }
 
 }
--- test/contracts/simple-wallet-test.js
+++ test/contracts/simple-wallet-test.js
@@ -5,10 +5,12 @@ describe('SimpleWallet Contract', function () {
     
     let simpleWalletContract;
     let simpleWalletDeployed;
+    let ethersProvider;
 
     beforeEach(async function () {
         simpleWalletContract = await ethers.getContractFactory('SimpleWallet');
         simpleWalletDeployed = await simpleWalletContract.deploy();
+        ethersProvider = ethers.provider;
 
         await simpleWalletDeployed.deployed();
     });
@@ -19,4 +21,53 @@ describe('SimpleWallet Contract', function () {
             expect(await simpleWalletDeployed.owner()).to.equal(expectedOwner.address);
         });
     });
+
+    describe('Fund wallet', function() {
+        it('should receive funds from an external account', async function() {
+            [owner, externalAccount] = await ethers.getSigners();
+
+            const tx = await externalAccount.sendTransaction({
+                to: simpleWalletDeployed.address,
+                value: ethers.utils.parseEther("0.1")
+            });
+
+            const contractBalance = await ethersProvider.getBalance(simpleWalletDeployed.address);
+            expect(ethers.utils.formatEther(contractBalance)).to.equal('0.1');
+            await expect(tx)
+                .to.emit(simpleWalletDeployed, 'FundsReceived')
+                .withArgs(externalAccount.address, ethers.utils.parseEther("0.1"));
+        });
+    });
+
+    describe('Withdrawals', function() {
+        it('should set one address allowed to withdraw funds', async function() {
+            [owner, allowedAccount] = await ethers.getSigners();
+            
+            const tx = await simpleWalletDeployed.addAllowedAccount(allowedAccount.address);
+
+            await expect(tx)
+                .to.emit(simpleWalletDeployed, 'NewAccountAllowed')
+                .withArgs(allowedAccount.address);
+            expect(await simpleWalletDeployed.isAllowed(allowedAccount.address))
+                .to.equal(true);
+        });
+
+        it('should fail if an external account try to set one address as allowed', async function() {
+            [owner, externaAccount, allowedAccount] = await ethers.getSigners();
+            
+            const tx = simpleWalletDeployed
+                                .connect(externaAccount)
+                                .addAllowedAccount(allowedAccount.address);
+
+            await expect(tx).to.be.revertedWith('Ownable: caller is not the owner');
+        });
+
+        it('should not be an allowed account', async function() {
+            [owner, notAllowedAccount] = await ethers.getSigners();
+            
+            expect(await simpleWalletDeployed.isAllowed(notAllowedAccount.address))
+                .to.equal(false);
+        });
+    });
+
 });
